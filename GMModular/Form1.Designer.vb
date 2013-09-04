aace92931d431aa89ae5cb093cce8aeecb25df98
RobQuistNL <rob.quist@dukesoft.nl>
2013-09-04 22:35:45 +0200

    View list

--- GMModular/Form1.Designer.vb
+++ GMModular/Form1.Designer.vb
@@ -22,14 +22,23 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Button1 = New System.Windows.Forms.Button()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.projectFolderTextbox = New System.Windows.Forms.TextBox()
-        Me.submoduleListBox = New System.Windows.Forms.ListBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.submoduleListBox = New System.Windows.Forms.ListView()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.check = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.projectfile = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
@@ -47,16 +56,16 @@ Partial Class Form1
         '
         'RichTextBox1
         '
-        Me.RichTextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.RichTextBox1.Location = New System.Drawing.Point(15, 12)
         Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(572, 96)
+        Me.RichTextBox1.Size = New System.Drawing.Size(551, 96)
         Me.RichTextBox1.TabIndex = 1
         Me.RichTextBox1.Text = ""
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 115)
+        Me.Label1.Location = New System.Drawing.Point(12, 119)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(72, 13)
         Me.Label1.TabIndex = 2
@@ -64,18 +73,10 @@ Partial Class Form1
         '
         'projectFolderTextbox
         '
-        Me.projectFolderTextbox.Location = New System.Drawing.Point(136, 112)
+        Me.projectFolderTextbox.Location = New System.Drawing.Point(118, 112)
         Me.projectFolderTextbox.Name = "projectFolderTextbox"
         Me.projectFolderTextbox.Size = New System.Drawing.Size(448, 20)
         Me.projectFolderTextbox.TabIndex = 3
-        '
-        'submoduleListBox
-        '
-        Me.submoduleListBox.FormattingEnabled = True
-        Me.submoduleListBox.Location = New System.Drawing.Point(136, 139)
-        Me.submoduleListBox.Name = "submoduleListBox"
-        Me.submoduleListBox.Size = New System.Drawing.Size(448, 95)
-        Me.submoduleListBox.TabIndex = 4
         '
         'Label2
         '
@@ -86,21 +87,93 @@ Partial Class Form1
         Me.Label2.TabIndex = 5
         Me.Label2.Text = "Current submodules"
         '
+        'submoduleListBox
+        '
+        Me.submoduleListBox.CheckBoxes = True
+        Me.submoduleListBox.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.check, Me.projectfile})
+        Me.submoduleListBox.FullRowSelect = True
+        Me.submoduleListBox.Location = New System.Drawing.Point(118, 138)
+        Me.submoduleListBox.Name = "submoduleListBox"
+        Me.submoduleListBox.Size = New System.Drawing.Size(448, 97)
+        Me.submoduleListBox.SmallImageList = Me.ImageList1
+        Me.submoduleListBox.TabIndex = 6
+        Me.submoduleListBox.UseCompatibleStateImageBehavior = False
+        Me.submoduleListBox.View = System.Windows.Forms.View.Details
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit
+        Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'check
+        '
+        Me.check.Text = ""
+        Me.check.Width = 39
+        '
+        'projectfile
+        '
+        Me.projectfile.Tag = ""
+        Me.projectfile.Text = "Project file"
+        Me.projectfile.Width = 405
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Shield = OK"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 51)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(67, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Cross = Error"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 34)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(139, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Exclamation = Need to sync"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Location = New System.Drawing.Point(3, 244)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Legend"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(597, 456)
-        Me.Controls.Add(Me.Label2)
+        Me.ClientSize = New System.Drawing.Size(572, 456)
         Me.Controls.Add(Me.submoduleListBox)
         Me.Controls.Add(Me.projectFolderTextbox)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.RichTextBox1)
         Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.Text = "GMModular"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -110,7 +183,14 @@ Partial Class Form1
     Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents projectFolderTextbox As System.Windows.Forms.TextBox
-    Friend WithEvents submoduleListBox As System.Windows.Forms.ListBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents submoduleListBox As System.Windows.Forms.ListView
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents check As System.Windows.Forms.ColumnHeader
+    Friend WithEvents projectfile As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
 
 End Class
--- GMModular/Form1.vb
+++ GMModular/Form1.vb
@@ -1,11 +1,8 @@
 ﻿Imports System
 Imports System.IO
+Imports System.Xml
 
 Public Class Form1
-
-    Private Sub FolderBrowserDialog1_HelpRequest(ByVal sender As System.Object, ByVal e As System.EventArgs)
-
-    End Sub
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         cleanUpProject()
@@ -45,23 +42,32 @@ Public Class Form1
         'Dim colFolders As Collection
         Dim submoduleFolder As String
         submoduleFolder = projectFolderTextbox.Text + "\submodules\"
+        If Not (My.Computer.FileSystem.FileExists(submoduleFolder + "modules.gmm")) Then
+            MessageBox.Show("modules.gmm file was not found in the submodules directory." + vbNewLine + vbNewLine + _
+                            "This file keeps track of all changes. It can be possible that you've just made the submodules folder, in that case, you're safe." + vbNewLine + vbNewLine + _
+                            "Otherwise, you should manually clean up your Original GMX project (remove all module stuff), or find the modules.gmm file! ", "modules.gmm not found", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
 
         If (My.Computer.FileSystem.DirectoryExists(submoduleFolder) = True) Then
             'Loop through the submodule folder to search for GMX files.
 
             Dim di As New DirectoryInfo(submoduleFolder)
             Dim diArr As DirectoryInfo() = di.GetDirectories()
+            Dim i As Integer = 0
 
             Dim dir As DirectoryInfo
             For Each dir In diArr
                 Dim gmxArr As FileInfo() = dir.GetFiles("*.gmx")
                 Dim gmxFile As FileInfo
 
+
                 For Each gmxFile In gmxArr
                     'Fetch parent directory name of GMX file (in case 2 GMX's have the same name.
                     Dim directoryName As String = Path.GetDirectoryName(gmxFile.FullName)
                     Dim parentName As String = Path.GetFileName(directoryName)
-                    submoduleListBox.Items.Add(parentName + "/" + gmxFile.Name)
+                    submoduleListBox.Items.Add("", "check")
+                    submoduleListBox.Items(i).SubItems.Add(parentName + "/" + gmxFile.Name)
+                    i = i + 1
                 Next (gmxFile)
             Next dir
 
@@ -71,5 +77,23 @@ Public Class Form1
     Private Sub cleanUpProject()
         submoduleListBox.Items.Clear()
     End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'Load images
+        ImageList1.Images.Add("check", System.Drawing.SystemIcons.Shield)
+        ImageList1.Images.Add("error", System.Drawing.SystemIcons.Error)
+        ImageList1.Images.Add("warning", System.Drawing.SystemIcons.Exclamation)
+
+    End Sub
+
+    Private Sub writeGMM(ByVal filename As String)
+        Dim settings As New XmlWriterSettings()
+        settings.Indent = True
+        Dim XmlWrt As XmlWriter = XmlWriter.Create(filename, settings)
+
+
+
+    End Sub
+
 
 End Class
